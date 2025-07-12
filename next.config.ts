f0f32df5173786fcfde19d41cd89a05fb70d4e3c
Sergei Pankov <sergeipankov98@mail.ru>
2025-07-13 01:36:01 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Автоматически определяем, если это GitHub Pages
-  ...(process.env.GITHUB_ACTIONS && {
+  // Применяем basePath только для GitHub Pages
+  ...(process.env.NODE_ENV === 'production' && {
     basePath: '/your-repo-name',
     assetPrefix: '/your-repo-name/',
   }),
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   output: 'export',
   trailingSlash: true,
   images: {
